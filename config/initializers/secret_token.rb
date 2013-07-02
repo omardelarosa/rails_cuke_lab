266b6cc376eff28508561a69939936aacca0858b
omar delarosa <thedelarosa@gmail.com>
2013-07-02 12:02:17 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCukeLab::Application.config.secret_token = '33f69f4e8dfb77d0a83f5b1c978abc0e9803f1755cb03974ee14126479383f292ff2794353da54055eb0bf3dc3fb73c90599a3d7def4888ccacfe156dc3f1a27'
