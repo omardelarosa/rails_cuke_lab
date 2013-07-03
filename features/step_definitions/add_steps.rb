7cdc1a92aa55d69f1606a075f79876bd3b75549c
omar delarosa <thedelarosa@gmail.com>
2013-07-03 19:33:29 -0400

    fixed problem with browser.close in step defs

--- features/step_definitions/add_steps.rb
+++ features/step_definitions/add_steps.rb
@@ -1,9 +1,6 @@
-#added watir
 require 'watir'
 browser = Watir::Browser.new :chrome
 url = "http://localhost:3000"
-
-#begin the step definitions
 
 Given(/^I visit the calculator page for add$/) do
   browser.goto "#{url}/add" # express the regexp above with the code you wish you had
@@ -13,6 +10,3 @@ Given(/^I fill in '(\d+)' for 'first'$/) do |number|
   browser.text_field(:id => 'first').set number # express the regexp above with the code you wish you had
 end
 
-
-#close browsers after testing
-browser.close
